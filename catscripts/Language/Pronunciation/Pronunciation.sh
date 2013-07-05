1c32cf96c4d59f768222ba40d7280e9fe84eba14
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:33:21 -0400

    Fr

--- catscripts/Language/Pronunciation/Pronunciation.sh
+++ catscripts/Language/Pronunciation/Pronunciation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KEYWORDS_PRONUNCIATION="How do you pronounce|How is .+ pronounced|pron(|o)unciation"
+KEYWORDS_PRONUNCIATION="Comment prononcez-vous|Comment est .+ prononc(ez|é|e|ée|és|ées)|prononciation|Comment pronounc(ez|é|e|és|ée|ées)"
 
 egrep -i "$KEYWORDS_PRONUNCIATION" newpages.txt >> Pronunciation.txt
 
@@ -9,7 +9,7 @@ PRONUNCIATION=`stat --print=%s Pronunciation.txt`
 if [ $PRONUNCIATION -ne 0 ];
 then
   export CATFILE="Pronunciation.txt"
-  export CATNAME="Pronunciation"
+  export CATNAME="Prononciation"
   $CATEGORIZE
 fi
 

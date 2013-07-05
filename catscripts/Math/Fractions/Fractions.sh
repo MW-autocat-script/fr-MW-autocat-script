ff458211b1aafc3f611b623618c370787cfd1d5f
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:52:36 -0400

    Fr

--- catscripts/Math/Fractions/Fractions.sh
+++ catscripts/Math/Fractions/Fractions.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-egrep -i 'as a fraction\]\]|into a fraction\]\]' newpages.txt > Fractions.txt
-egrep -i 'in simplest form\]\]' newpages.txt >> Fractions.txt
-egrep -i 'in its simplest form\]\]' newpages.txt >> Fractions.txt
-egrep -i 'in lowest terms\]\]|reduced fraction' newpages.txt >> Fractions.txt
+egrep -i 'dans une fraction\]\]|transform(é|e|er|ée|ées|és)\]\]' newpages.txt > Fractions.txt
+egrep -i 'dans la forme la plus simple\]\]' newpages.txt >> Fractions.txt
+egrep -i 'fraction réduite' newpages.txt >> Fractions.txt
 egrep -i 'simplest form of [0-9]{1,}/[0-9]{1,}' newpages.txt >> Fractions.txt
-egrep -i '[0-9]{1,}(/|\\)[0-9]{1,} of [0-9]{1,}' newpages.txt >> Fractions.txt
-egrep -i '[0-9]{1,}(/|\\)[0-9]{1,}(| )(plus|minus|times|divided(| )by)(| )[0-9]{1,}(/|\\)[0-9]{1,}' newpages.txt >> Fractions.txt
-egrep -i '[0-9]{1,}(/|\\)[0-9]{1,}(| )(plus|minus|times|divided(| )by)(| )[0-9]{1.} [0-9]{1,}(/|\\)[0-9]{1,}' newpages.txt >> Fractions.txt
+egrep -i '[0-9]{1,}(/|\\)[0-9]{1,} de [0-9]{1,}' newpages.txt >> Fractions.txt
+egrep -i '[0-9]{1,}(/|\\)[0-9]{1,}(| )(plus|moins|fois|divis(é|e|er|ée|ées|és)(| )par)(| )[0-9]{1,}(/|\\)[0-9]{1,}' newpages.txt >> Fractions.txt
+egrep -i '[0-9]{1,}(/|\\)[0-9]{1,}(| )(plus|moins|fois|divis(é|e|er|ée|ées|és)(| )par)(| )[0-9]{1.} [0-9]{1,}(/|\\)[0-9]{1,}' newpages.txt >> Fractions.txt
 
 FRACTIONS=`stat --print=%s Fractions.txt`
 

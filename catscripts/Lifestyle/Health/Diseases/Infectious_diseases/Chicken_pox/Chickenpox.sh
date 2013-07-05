7505892df81c5e9c80535e4cc81571d973d61541
JrMime <vinc.bel@hotmail.com>
2013-07-05 12:37:22 -0400

    Fr

--- catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Chicken_pox/Chickenpox.sh
+++ catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Chicken_pox/Chickenpox.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'chicken(| )pox' newpages.txt >> Chickenpox.txt
+egrep -i 'varicelle' newpages.txt >> Chickenpox.txt
 
 CHICKENPOX=`stat --print=%s Chickenpox.txt`
 
 if [ $CHICKENPOX -ne 0 ];
 then
   export CATFILE="Chickenpox.txt"
-  export CATNAME="Chicken pox"
+  export CATNAME="Varicelle"
   $CATEGORIZE
 fi
 

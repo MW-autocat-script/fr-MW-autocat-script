8832d9c93cb051c767ea65c8b1f2a7a5d80575fa
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:41:43 -0400

    Fr

--- catscripts/Language/Spelling/Spelling.sh
+++ catscripts/Language/Spelling/Spelling.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-egrep -i '\[\[How do you spell [a-z]{1,}\]\]' newpages.txt >> Spelling.txt
-egrep -i '\[\[How do you spell "[a-z]{1,}"\]\]' newpages.txt >> Spelling.txt
-egrep -i "\[\[How do you spell '[a-z]{1,}'\]\]" newpages.txt >> Spelling.txt
-egrep -i '\[\[How to spell [a-z]{1,}\]\]' newpages.txt >> Spelling.txt
-egrep -i '\[\[How to spell "[a-z]{1,}"\]\]' newpages.txt >> Spelling.txt
-egrep -i "\[\[How to spell '[a-z]{1,}'\]\]" newpages.txt >> Spelling.txt
+egrep -i '\[\[Comment épelez-vous [a-z]{1,}\]\]' newpages.txt >> Spelling.txt
+egrep -i '\[\[Comment épelez-vous "[a-z]{1,}"\]\]' newpages.txt >> Spelling.txt
+egrep -i "\[\[Comment épelez-vous '[a-z]{1,}'\]\]" newpages.txt >> Spelling.txt
+egrep -i '\[\[Comment épel(é|er|ez|ée|ées|és) [a-z]{1,}\]\]' newpages.txt >> Spelling.txt
+egrep -i '\[\[Comment épel(é|er|ez|ée|ées|és) "[a-z]{1,}"\]\]' newpages.txt >> Spelling.txt
+egrep -i "\[\[HComment épel(é|er|ez|ée|ées|és) '[a-z]{1,}'\]\]" newpages.txt >> Spelling.txt
 
 SPELLING=`stat --print=%s Spelling.txt`
 
 if [ $SPELLING -ne 0 ];
 then
   export CATFILE="Spelling.txt"
-  export CATNAME="Spelling"
+  export CATNAME="Orthographe"
   $CATEGORIZE
 fi
 

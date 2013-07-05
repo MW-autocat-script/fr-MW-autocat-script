3ce7e9ed83f38f8f134d4c51be37f7b3846a290c
JrMime <vinc.bel@hotmail.com>
2013-07-05 13:29:14 -0400

    Fr

--- catscripts/Science/Astronomy/Moon/Moon.sh
+++ catscripts/Science/Astronomy/Moon/Moon.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i "\bMoon\b|\bMoon's|lunar eclipse|solar eclipse" newpages.txt | egrep -iv 'Sailor Moon|Type( |-)Moon|Harvest Moon|moon stone|Twilight|Pokémon|Pokemon|Jacob|Bella|Edward|RuneScape' >> Moon.txt
+egrep -i "\bLune\b|(é|e)clipse solaire" newpages.txt | egrep -iv 'Sailor Moon|Type( |-)Moon|Harvest Moon|moon stone|Twilight|Pokémon|Pokemon|Jacob|Bella|Edward|RuneScape' >> Moon.txt
 
 MOON=`stat --print=%s Moon.txt`
 
 if [ $MOON -ne 0 ];
 then
   export CATFILE="Moon.txt"
-  export CATNAME="Moon"
+  export CATNAME="Lune"
   $CATEGORIZE
 fi
 

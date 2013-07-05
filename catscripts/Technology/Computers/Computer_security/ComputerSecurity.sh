bf37c1464562baf50e44b270a20659c5cf95de16
JrMime <vinc.bel@hotmail.com>
2013-07-05 14:03:28 -0400

    Fr

--- catscripts/Technology/Computers/Computer_security/ComputerSecurity.sh
+++ catscripts/Technology/Computers/Computer_security/ComputerSecurity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KEYWORDS_COMPUTER_SECURITY="Computer(| )Security|\bhack(er(|s)|ing|s|ed)\b|CompSec|(ad|spy|mal)(|-)ware|computer.+virus|virus.+computer|trojan|SQL(| )injection|DNSSEC|IPSEC"
+KEYWORDS_COMPUTER_SECURITY="S(é|e)curit(é|er|e)(| )(d|)('|)(ordinateur|)|\bhack(er(|s)|ing|s|ed)\b|CompSec|(ad|spy|mal)(|-)ware|computer.+virus|virus.+computer|trojan|SQL(| )injection|DNSSEC|IPSEC"
 KEYWORDS_COMPUTER_SECURITY_EXCLUDE="anti(|-)(| )virus|\bTroy|Greek|Trojan(| )War|USC(| )Trojans|sports|football|tennis"
 
 egrep -i "$KEYWORDS_COMPUTER_SECURITY" newpages.txt | egrep -iv "$KEYWORDS_COMPUTER_SECURITY_EXCLUDE" >> ComputerSecurity.txt
@@ -10,7 +10,7 @@ SECURITY=`stat --print=%s ComputerSecurity.txt`
 if [ $SECURITY -ne 0 ];
 then
   export CATFILE="ComputerSecurity.txt"
-  export CATNAME="Computer security"
+  export CATNAME="Sécurité d'ordinateur"
   $CATEGORIZE
 fi
 

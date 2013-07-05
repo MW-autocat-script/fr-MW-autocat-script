508f973b655a0950b35456875ba395e7ac0285b4
JrMime <vinc.bel@hotmail.com>
2013-07-05 14:16:34 -0400

    Fr

--- catscripts/Technology/Computers/Software/Office_applications/Officeapplications.sh
+++ catscripts/Technology/Computers/Software/Office_applications/Officeapplications.sh
@@ -2,7 +2,7 @@
 
 egrep -i 'Office suite|office application' newpages.txt | egrep -iv 'Microsoft Office' >> Officeapps.txt
 egrep -i 'Open(| )Office|Libre(| )Office' newpages.txt >> Officeapps.txt
-egrep -i 'word process(or|ing)' newpages.txt | egrep -iv 'Microsoft Word' >> Wordprocessing.txt
+egrep -i 'traitement(s|) de text(e|)|traiteur(s|) de text(e|)' newpages.txt | egrep -iv 'Microsoft Word' >> Wordprocessing.txt
 
 APPS=`stat --print=%s Officeapps.txt`
 WPROCESS=`stat --print=%s Wordprocessing.txt`
@@ -17,7 +17,7 @@ fi
 if [ $WPROCESS -ne 0 ];
 then
   export CATFILE="Wordprocessing.txt"
-  export CATNAME="Word processors"
+  export CATNAME="Traiteurs de texte"
   $CATEGORIZE
 fi
 

1c5f723b6990e664624888a7deb42c0d40f1504e
JrMime <vinc.bel@hotmail.com>
2013-07-05 13:32:37 -0400

    Fr

--- catscripts/Science/Astronomy/Astronomy.sh
+++ catscripts/Science/Astronomy/Astronomy.sh
@@ -6,14 +6,14 @@ $CURRENTDIR/Dwarf_planets/Dwarfplanets.sh
 $CURRENTDIR/Planets/Planets.sh
 $CURRENTDIR/Moon/Moon.sh
 
-egrep -i 'Asteroid' newpages.txt | egrep -iv 'download|game' >> Asteroids.txt
+egrep -i 'Asteroid|Ast(é|e)ro(ï|i)de' newpages.txt | egrep -iv 'download|game|jeux' >> Asteroids.txt
 
 ASTEROIDS=`stat --print=%s Asteroids.txt`
 
 if [ $ASTEROIDS -ne 0 ];
 then
   export CATFILE="Asteroids.txt"
-  export CATNAME="Asteroids"
+  export CATNAME="Astéroïdes"
   $CATEGORIZE
 fi
 

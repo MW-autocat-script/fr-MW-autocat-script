9c2efcb1b9daf357b08ef8bb63e1bcfefcd2e0a1
JrMime <vinc.bel@hotmail.com>
2013-07-05 12:39:20 -0400

    Fr

--- catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Flu/Flu.sh
+++ catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Flu/Flu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-egrep -i 'Flu\b|influenza' newpages.txt | egrep -iv 'H1N1|swine flu' >> Flu.txt
-egrep -i 'H1N1|Swine flu' newpages.txt >> SwineFlu.txt
+egrep -i 'grippe\b' newpages.txt | egrep -iv 'H1N1|grippe porcine' >> Flu.txt
+egrep -i 'H1N1|grippe porcine' newpages.txt >> SwineFlu.txt
 
 FLU=`stat --print=%s Flu.txt`
 SWINE=`stat --print=%s SwineFlu.txt`
@@ -9,14 +9,14 @@ SWINE=`stat --print=%s SwineFlu.txt`
 if [ $FLU -ne 0 ];
 then
   export CATFILE="Flu.txt"
-  export CATNAME="Flu"
+  export CATNAME="Grippe"
   $CATEGORIZE
 fi
 
 if [ $SWINE -ne 0 ];
 then
   export CATFILE="SwineFlu.txt"
-  export CATNAME="Swine flu"
+  export CATNAME="Grippe porcine"
   $CATEGORIZE
 fi
 

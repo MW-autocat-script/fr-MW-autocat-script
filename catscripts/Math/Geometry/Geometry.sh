5aedc2aa1f727e5c257155d5a33a40d55f68dba3
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:55:57 -0400

    Fr

--- catscripts/Math/Geometry/Geometry.sh
+++ catscripts/Math/Geometry/Geometry.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #Warning - the regex \bpi\b will fail on words with non-ASCII characters, such as piñata
-egrep -i '(|plane) geometr(y|ic)|polygon|(obtuse|acute|right) angle|isosceles|equilateral|triangle|square\b|rectangle|trapezoid|pentagon|tor(us|oid)|rhomb(us|oid)|hexagon|sexagon|septagon|octagon|nonagon|(|do)decagon|dodecahedron|AAA postulate|SAS postulate|pythagorean theorem|\bpi |\bpi\]\]|π|Euclid' newpages.txt | egrep -iv "punnett|root| inc(|\.)\b|squaresoft|square number|bermuda triangle|Square Enix|town square|St\. .+ Square|Trafalgar Square|Raspberry Pi|Parliament Square|Red Square|Tiananmen Square|square danc(e|ing)|St\. Mark's Square|piñata|square deal|chess|Rune(| )Scape" | egrep -v 'Pentagon' >> Geometry.txt
+egrep -i '(|plane) g(é|e)om(é|e)trie|polygone|angle (obstu|acute|droit|rectagle)|isoscèle|équilatérale|triangle|carré(e|)\b|rectangle|trapezoïd|pentagone|tor(us|oid)|rhomb(us|oid)|hexagone|sexagone|septagone|octagone|nonagone|(|do)decagone|dodecahedrone|AAA postulate|SAS postulate|pythagorean theorem|\bpi |\bpi\]\]|π|Euclid' newpages.txt | egrep -iv "punnett|root| inc(|\.)\b|squaresoft|carré(e|) cupique|bermuda triangle|Square Enix|town square|St\. .+ Square|Trafalgar Square|Raspberry Pi|Parliament Square|Red Square|Tiananmen Square|square danc(e|ing)|St\. Mark's Square|piñata|square deal|chess|Rune(| )Scape" | egrep -v 'Pentagon' >> Geometry.txt
 
 GEOMETRY=`stat --print=%s Geometry.txt`
 
 if [ $GEOMETRY -ne 0 ];
 then
   export CATFILE="Geometry.txt"
-  export CATNAME="Geometry"
+  export CATNAME="Géométrie"
   $CATEGORIZE
 fi
 

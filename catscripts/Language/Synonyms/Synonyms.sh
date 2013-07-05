d7cd7cfbdfac8f7e9de9f980624d3c12a6ca2568
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:43:55 -0400

    Fr

--- catscripts/Language/Synonyms/Synonyms.sh
+++ catscripts/Language/Synonyms/Synonyms.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-egrep -i "(What is|What's|Whats) another word for [a-z]{1,}" newpages.txt >> Synonyms.txt
-egrep -i "(What is|What's|Whats) a synonym for [a-z]{1,}" newpages.txt >> Synonyms.txt
-egrep -i "\[\[Another word for [a-z]{1,}\]\]" newpages.txt >> Synonyms.txt
+egrep -i "Quel(le|) est un autre mot pour[a-z]{1,}" newpages.txt >> Synonyms.txt
+egrep -i "Quel(le|) est un synonyme pour [a-z]{1,}" newpages.txt >> Synonyms.txt
+egrep -i "\[\[(Un|) (a|A)utre mot pour [a-z]{1,}\]\]" newpages.txt >> Synonyms.txt
 
 SYNONYMS=`stat --print=%s Synonyms.txt`
 
 if [ $SYNONYMS -ne 0 ];
 then
   export CATFILE="Synonyms.txt"
-  export CATNAME="Synonyms"
+  export CATNAME="Synonymes"
   $CATEGORIZE
 fi
 

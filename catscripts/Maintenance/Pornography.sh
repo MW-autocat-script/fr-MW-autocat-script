8ba3ae8c6d92b027ca1b9d47fdd417f47ab2d98e
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:50:12 -0400

    Fr

--- catscripts/Maintenance/Pornography.sh
+++ catscripts/Maintenance/Pornography.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'porn|hentai|lolicon|futanari|sex.+videos|videos.+sex' newpages.txt >> Pornography.txt
+egrep -i 'porn|hentai|lolicon|futanari|sex.+vidéos|vidéos.+sex' newpages.txt >> Pornography.txt
 
 PORN=`stat --print=%s Pornography.txt`
 
 if [ $PORN -ne 0 ];
 then
   export CATFILE="Pornography.txt"
-  export CATNAME="Pornography"
+  export CATNAME="Pornographie"
   $CATEGORIZE
 fi
 

97564820bc9e3de50259c2928ec216feb36f59f3
JrMime <vinc.bel@hotmail.com>
2013-07-05 13:02:31 -0400

    Fr

--- catscripts/Maintenance/Noninterrogative.sh
+++ catscripts/Maintenance/Noninterrogative.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -iv "\bwho(|s)\b" newpages.txt| egrep -iv "\bwhat" | egrep -iv "\bwhere\b" | egrep -iv "\bwhen\b" | egrep -iv "(^|\[\[|(\.|,) )Whens\b" | egrep -iv "\bwhy" | egrep -iv "\bhow\b" | egrep -iv "(^|\[\[|(\.|,) )Do" | egrep -iv "(^|\[\[|(\.|,) )has\b" | egrep -iv "(^|\[\[|\. |, )does\b" | egrep -iv "did\b" | egrep -iv "(^|\[\[|\. |, )Will\b" | egrep -iv "(^|\[\[|(\.|,) )Is\b" | egrep -iv ", is\b" | egrep -iv "(^|\[\[|(\.|,) )Are\b" | egrep -iv "(^|\[\[|(\.|,) )Aren't\b" | egrep -iv "(^|\[\[|(\.|,) )were\b" | egrep -iv '(^|\[\[|(\.|,) )was\b' | egrep -iv "(^|\[\[|(\.|,) )which\b" | egrep -iv "(^|\[\[|(\.|,) )could" | egrep -iv "(^|\[\[|(\.|,) )should\b" | egrep -iv "(^|\[\[|(\.|,) )shouldn(|')t" | egrep -iv "(^|\[\[|(\.|,) )would" | egrep -iv "(^|\[\[|\. |, )can\b" | egrep -iv "(^|\[\[|(\.|,) )Has\b" | egrep -iv "(^|\[\[|(\.|,) )Hasn't" | egrep -iv "\bAm I\b" | egrep -iv "(^|\[\[|(\.|,) )Have(|n(|')t)\b" | egrep -iv "(^|\[\[|(\.|,) )(Isn(|')t)" | egrep -iv "(^|\[\[|(\.|,) )Doesn(|')t\b" | egrep -iv "(^|\[\[|(\.|,) )Didn(|')t" >> noninterrogative.txt
+egrep -iv "\bqui(|s)\b" newpages.txt| egrep -iv "\bquoi" | egrep -iv "\boù\b" | egrep -iv "\wbquand\b" | egrep -iv "(^|\[\[|(\.|,) )Quand\b" | egrep -iv "\bpourquoi" | egrep -iv "\bcomment\b" | egrep -iv "(^|\[\[|(\.|,) )Est(-|)(| )ce(-|)(| )qu" | egrep -iv "\bfait\b" | egrep -iv "(^|\[\[|(\.|,) )Est(-| |)t(-| |)(il|elle)\b" | egrep -iv "(^|\[\[|(\.|,) )(é|e)tai(t|s)(-| |)t(-| |)(il|elle)\b" | egrep -iv '(^|\[\[|(\.|,) )(é|e)tai(t|s)\b' | egrep -iv "(^|\[\[|(\.|,) )quel(le|)\b" | egrep -iv "(^|\[\[|(\.|,) )devrai" | egrep -iv "(^|\[\[|(\.|,) )ne devrait pas" | egrep -iv "(^|\[\[|(\.|,) )voudrai(t|s|ent)" | egrep -iv "(^|\[\[|\. |, )peu(t|s)\b" | egrep -iv "\bsuis(-| |)je\b" | egrep -iv "(^|\[\[|(\.|,) )avoir\b" | egrep -iv "(^|\[\[|(\.|,) )est" | egrep -iv "(^|\[\[|(\.|,) )(n'|n )est(-| |)(c|s)e\b" >> noninterrogative.txt
 
 NONINT=`stat --print=%s noninterrogative.txt`
 
 if [ $NONINT -ne 0 ];
 then
   export CATFILE="noninterrogative.txt"
-  export CATNAME="Page titles lacking an interrogative word"
+  export CATNAME="Titres de pages qui n'ont pas un mot interrogatif"
   $CATEGORIZE
 fi
 

81a68614e4c2caecc94a3a38eabc3ea291b1cfcb
JrMime <vinc.bel@hotmail.com>
2013-07-05 14:49:04 -0400

    Fr

--- catscripts/Entertainment/Music/Lyrics/Lyrics.sh
+++ catscripts/Entertainment/Music/Lyrics/Lyrics.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'lyrics|lyric\b' newpages.txt >> Lyrics.txt
+egrep -i 'lyriques' newpages.txt >> Lyrics.txt
 
 LYRICS=`stat --print=%s Lyrics.txt`
 
 if [ $LYRICS -ne 0 ];
 then
   export CATFILE="Lyrics.txt"
-  export CATNAME="Lyrics"
+  export CATNAME="Lyriques"
   $CATEGORIZE
 fi
 

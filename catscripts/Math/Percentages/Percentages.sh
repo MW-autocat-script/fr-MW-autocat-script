79c4a1e04827cacd00e0a5ec7093d9790d8e13b0
JrMime <vinc.bel@hotmail.com>
2013-07-05 13:24:15 -0400

    Fr

--- catscripts/Math/Percentages/Percentages.sh
+++ catscripts/Math/Percentages/Percentages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-egrep -i '[0-9]{1,} is what percent of [0-9]{1,}' newpages.txt >> Percentages.txt
-egrep -i '\$[0-9]{1,} is what percent of \$[0-9]{1,}' newpages.txt >> Percentages.txt
-egrep -i '\$[0-9]{1,}\.[0-9]{1,} is what percent of \$[0-9]{1,}\.[0-9]{1,}' newpages.txt >> Percentages.txt
-egrep -i '^What( is|s) [0-9]{1,}(| )% of [0-9]{1,}' newpages.txt >> Percentages.txt
-egrep -i 'What percent of [0-9]{1,} is [0-9]{1,}' newpages.txt >> Percentages.txt
+egrep -i '[0-9]{1,} est combien de pour( |)cent(age|) [0-9]{1,}' newpages.txt >> Percentages.txt
+egrep -i '\$[0-9]{1,} est combien de pour( |)cent(age|) \$[0-9]{1,}' newpages.txt >> Percentages.txt
+egrep -i '\$[0-9]{1,}\.[0-9]{1,} est combien de pour( |)cent(age|) \$[0-9]{1,}\.[0-9]{1,}' newpages.txt >> Percentages.txt
+egrep -i "^C('est) quoi [0-9]{1,}(| )% de [0-9]{1,}" newpages.txt >> Percentages.txt
+egrep -i 'Combien de pour( |)cent(age|) est [0-9]{1,} de [0-9]{1,}' newpages.txt >> Percentages.txt
 
 
 PERCENTAGES=`stat --print=%s Percentages.txt`
@@ -12,7 +12,7 @@ PERCENTAGES=`stat --print=%s Percentages.txt`
 if [ $PERCENTAGES -ne 0 ];
 then
   export CATFILE="Percentages.txt"
-  export CATNAME="Percentages"
+  export CATNAME="Pourcentages"
   $CATEGORIZE
 fi
 

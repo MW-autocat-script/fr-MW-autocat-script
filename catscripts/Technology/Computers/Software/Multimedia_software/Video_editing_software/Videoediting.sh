5d29378fd38176ff8e66263d6dce3f4193096624
JrMime <vinc.bel@hotmail.com>
2013-07-05 14:08:41 -0400

    Fr

--- catscripts/Technology/Computers/Software/Multimedia_software/Video_editing_software/Videoediting.sh
+++ catscripts/Technology/Computers/Software/Multimedia_software/Video_editing_software/Videoediting.sh
@@ -4,7 +4,7 @@ egrep -i 'Windows(|(| )Live)(| )Movie(| )Maker' newpages.txt >> WindowsMovieMake
 egrep -i 'Adobe(| )Premiere|Premier(| )Pro' newpages.txt >> AdobePremierePro.txt
 egrep -i '\biMovie|Nero Vision' newpages.txt >> Videoediting.txt
 egrep -i 'Corel Video(| )Studio' newpages.txt >> Videoediting.txt
-egrep -i 'video edit|edit video|convert.+video|video.+convert' newpages.txt | egrep -iv 'Windows(|(| )Live)(| )Movie(| )Maker|Adobe(| )Premiere|Premier(| )Pro|\biMovie' >> Videoediting.txt
+egrep -i 'modification(s|) de vid(é|e)o|vid(é|e)o edit|edit vid(é|e)o|convert.+vid(é|e)o|vid(é|e)o.+convert' newpages.txt | egrep -iv 'Windows(|(| )Live)(| )Movie(| )Maker|Adobe(| )Premiere|Premier(| )Pro|\biMovie' >> Videoediting.txt
 
 WMM=`stat --print=%s WindowsMovieMaker.txt`
 PREMIERE=`stat --print=%s AdobePremierePro.txt`
@@ -13,7 +13,7 @@ VIDEO=`stat --print=%s Videoediting.txt`
 if [ $VIDEO -ne 0 ];
 then
   export CATFILE="Videoediting.txt"
-  export CATNAME="Video editing software"
+  export CATNAME="Logiciel de montage vidéo"
   $CATEGORIZE
 fi
 

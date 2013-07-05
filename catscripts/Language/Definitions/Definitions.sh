4045d0cbf90b8662d41b3f094a1a683217aed32b
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:15:26 -0400

    Fr

--- catscripts/Language/Definitions/Definitions.sh
+++ catscripts/Language/Definitions/Definitions.sh
@@ -5,27 +5,14 @@ then
   printf "Starting Definitions\n" 
 fi
 
-egrep -i '(^|\[\[)What is a(|n) [abcdefghijklmnopqrstuvwxyz]{1,} (|[abcdefghijklmnopqrstuvwxyz]{1,})($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What are [abcdefghijklmnopqrstuvwxyz]{1,} (|[abcdefghijklmnopqrstuvwxyz]{1,})($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What is the definition of [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What is the definition of a [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt  >> Definitions.txt
-egrep -i '(^|\[\[)Definition of [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)Defination of [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)Difinition of [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i "(^|\[\[)What's a [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])" newpages.txt >> Definitions.txt
-egrep -i "(^|\[\[)What's an [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])" newpages.txt>> Definitions.txt
-egrep -i '(^|\[\[)Whats a [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)Whats an [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What does [abcdefghijklmnopqrstuvwxyz]{1,} mean($|\]\])' newpages.txt >> Definitions.txt
+egrep -i "(^|\[\[)(C'est quoi un|Qu'est ce que (c'est|sait|sais|sait|s'est|ces|ses) (un|des)) [abcdefghijklmnopqrstuvwxyz]{1,} (|[abcdefghijklmnopqrstuvwxyz]{1,})($|\]\])" newpages.txt >> Definitions.txt
+egrep -i "(^|\[\[)Quelle est la définition (de|d'un) [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])" newpages.txt >> Definitions.txt
+egrep -i '(^|\[\[)D(é|e)finition(s|) de [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
+egrep -i "(^|\[\[)Qu'est-ce que [abcdefghijklmnopqrstuvwxyz]{1,} veut dire($|\]\])" newpages.txt >> Definitions.txt
 egrep -i '(^|\[\[)What does [abcdefghijklmnopqrstuvwxyz]{1,} [abcdefghijklmnopqrstuvwxyz]{1,} mean($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What is [abcdefghijklmnopqrstuvwxyz]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What is [a-z]{6,} [a-z]{6,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i 'What does the word [a-z]{1,} mean' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What are [a-z]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i '(^|\[\[)What are [a-z]{1,} [a-z]{1,}($|\]\])' newpages.txt >> Definitions.txt
-egrep -i 'What does the (sur|)name [a-z]{1,} mean' newpages.txt >> NameMeanings.txt
-egrep -i 'What does the (sur|)name "[a-z]{1,}" mean' newpages.txt >> NameMeanings.txt
-egrep -i "What does the (sur|)name '[a-z]{1,}' mean" newpages.txt >> NameMeanings.txt
+egrep -i "(^|\[\[)(C'est quoi un|Qu'est ce que (c'est|sait|sais|sait|s'est|ces|ses) (un|des)) [a-z]{6,} [a-z]{6,}($|\]\])" newpages.txt >> Definitions.txt
+egrep -i "(Qu'est ce que le mot|Le mot) [a-z]{1,} veut (dire|)( |)quoi" newpages.txt >> Definitions.txt
+egrep -i "Qu'est ce que le (sur|)nom [a-z]{1,} veut dire" newpages.txt >> NameMeanings.txt
 
 DEFINITIONS=`stat --print=%s Definitions.txt`
 NAMEMEANINGS=`stat --print=%s NameMeanings.txt`
@@ -33,14 +20,14 @@ NAMEMEANINGS=`stat --print=%s NameMeanings.txt`
 if [ $DEFINITIONS -ne 0 ];
 then
   export CATFILE="Definitions.txt"
-  export CATNAME="Definitions"
+  export CATNAME="Définitions"
   $CATEGORIZE
 fi
 
 if [ $NAMEMEANINGS -ne 0 ];
 then
   export CATFILE="NameMeanings.txt"
-  export CATNAME="Name meanings"
+  export CATNAME="Significations de noms"
   $CATEGORIZE
 fi
 

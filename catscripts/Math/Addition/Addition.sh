75d93becf17ca85972d1ff17e19689f7ef30f563
JrMime <vinc.bel@hotmail.com>
2013-07-05 13:05:22 -0400

    Fr

--- catscripts/Math/Addition/Addition.sh
+++ catscripts/Math/Addition/Addition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KEYWORDS_ADDITION="What is [0-9]{1,}(|(/|\.)[0-9]{1,}) (plus|\+) [0-9]{1,}(|(/|\.)[0-9]{1,})|What is [a-z]{1,}(|-[a-z]{1,}) plus [a-z]{1,}(|[a-z]{1,})"
+KEYWORDS_ADDITION="C('|)est quoi [0-9]{1,}(|(/|\.)[0-9]{1,}) (plus|\+) [0-9]{1,}(|(/|\.)[0-9]{1,})|C('|)est quoi [a-z]{1,}(|-[a-z]{1,}) plus [a-z]{1,}(|[a-z]{1,})"
 
 egrep -i "$KEYWORDS_ADDITION" newpages.txt >> Addition.txt
 

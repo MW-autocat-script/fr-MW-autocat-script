16f87524cbefd94df65c314dc1c21f2494529b38
JrMime <vinc.bel@hotmail.com>
2013-07-05 15:03:47 -0400

    Fr

--- catscripts/Language/Antonyms/Antonyms.sh
+++ catscripts/Language/Antonyms/Antonyms.sh
@@ -5,10 +5,10 @@ then
   printf "Starting Antonyms\n" 
 fi
 
-egrep -i "(What is|What's|Whats) the opposite of [a-z]{1,}" newpages.txt >> Antonyms.txt
-egrep -i "(What is|What's|Whats) (a|an) antonym of [a-z]{1,}" newpages.txt >> Antonyms.txt
-egrep -i "\[\[opposite of [a-z]{1,}\]\]" newpages.txt >> Antonyms.txt
-egrep -i "\[\[Antonym(|s) for [a-z]{1,}\]\]" newpages.txt >> Antonyms.txt
+egrep -i "Quel(le|) est l'oppos(é|e|er|ition) (de|du) [a-z]{1,}" newpages.txt >> Antonyms.txt
+egrep -i "Quel(le|) est l'antonyme de[a-z]{1,}" newpages.txt >> Antonyms.txt
+egrep -i "\[\[antonyme de [a-z]{1,}\]\]" newpages.txt >> Antonyms.txt
+egrep -i "\[\[Antonyme(s|) pour [a-z]{1,}\]\]" newpages.txt >> Antonyms.txt
 egrep -i 'antonym' newpages.txt >> Antonyms.txt
 
 ANTONYMS=`stat --print=%s Antonyms.txt`
@@ -16,7 +16,7 @@ ANTONYMS=`stat --print=%s Antonyms.txt`
 if [ $ANTONYMS -ne 0 ];
 then
   export CATFILE="Antonyms.txt"
-  export CATNAME="Antonyms"
+  export CATNAME="Antonymes"
   $CATEGORIZE
 fi
 

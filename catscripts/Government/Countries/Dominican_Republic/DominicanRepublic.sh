1cc42d5bdd8b3e3bbbf18160982fcd6c831e36c3
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 14:40:08 -0400

    Fr

--- catscripts/Government/Countries/Dominican_Republic/DominicanRepublic.sh
+++ catscripts/Government/Countries/Dominican_Republic/DominicanRepublic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Dominican(| )Republic' newpages.txt >> DominicanRepublic.txt
+egrep -i 'R(é|e)publique Dominicane' newpages.txt >> DominicanRepublic.txt
 
 DOMINICAN=`stat --print=%s DominicanRepublic.txt`
 
 if [ $DOMINICAN -ne 0 ];
 then
   export CATFILE="DominicanRepublic.txt"
-  export CATNAME="Dominican Republic"
+  export CATNAME="République Dominicane"
   $CATEGORIZE
 fi
 

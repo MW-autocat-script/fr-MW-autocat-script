c46e428e85004ccdf5c53d31c89440d22a678713
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:47:09 -0400

    Fr

--- catscripts/Government/Countries/Belgium/Belgium.sh
+++ catscripts/Government/Countries/Belgium/Belgium.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Belgium|Belgian' newpages.txt >> Belgium.txt
+egrep -i 'Belgique|Belgian' newpages.txt >> Belgium.txt
 
 BELGIUM=`stat --print=%s Belgium.txt`
 
 if [ $BELGIUM -ne 0 ];
 then
   export CATFILE="Belgium.txt"
-  export CATNAME="Belgium"
+  export CATNAME="Belgique"
   $CATEGORIZE
 fi
 

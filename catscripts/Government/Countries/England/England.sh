dd2ba6e99394168c9e1c3b12bc48c7def1b702c1
I am not me <impeccabletiming@gmail.com>
2013-07-07 15:33:51 -0400

    Update England.sh

--- catscripts/Government/Countries/England/England.sh
+++ catscripts/Government/Countries/England/England.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-egrep -i 'England|British' newpages.txt | egrep -iv 'British Isles|London|Church of England|New England|Stonehenge|British Columbia' >> England.txt #Since most people don't know the difference between British and English
+egrep -i 'England|British|Angleterre' newpages.txt | egrep -iv 'British Isles|London|Church of England|New England|Stonehenge|British Columbia' >> England.txt #Since most people don't know the difference between British and English
 egrep -i 'London' newpages.txt | egrep -iv 'Jack London' >> London.txt #Jack London was an American author
 egrep -i 'Stonehenge' newpages.txt >> Stonehenge.txt
 
@@ -11,7 +11,7 @@ STONEHENGE=`stat --print=%s Stonehenge.txt`
 if [ $ENGLAND -ne 0 ];
 then
   export CATFILE="England.txt"
-  export CATNAME="England"
+  export CATNAME="Angleterre"
   $CATEGORIZE
 fi
 

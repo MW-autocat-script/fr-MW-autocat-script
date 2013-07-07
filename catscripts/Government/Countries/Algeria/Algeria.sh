bdbab111cb7db71e75faf1eefe8233954f6dc927
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:40:39 -0400

    Fr

--- catscripts/Government/Countries/Algeria/Algeria.sh
+++ catscripts/Government/Countries/Algeria/Algeria.sh
@@ -6,14 +6,14 @@ then
   printf "Starting Algeria\n" 
 fi
 
-egrep -i 'Algeria' newpages.txt >> Algeria.txt
+egrep -i 'Alg(é|e)rie' newpages.txt >> Algeria.txt
 
 ALGERIA=`stat --print=%s Algeria.txt`
 
 if [ $ALGERIA -ne 0 ];
 then
   export CATFILE="Algeria.txt"
-  export CATNAME="Algeria"
+  export CATNAME="Algérie"
   $CATEGORIZE
 fi
 

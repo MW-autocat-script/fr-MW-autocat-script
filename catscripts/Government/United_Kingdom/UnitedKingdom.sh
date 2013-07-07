a2dccf08b1d3cd5561682bba92ed03a2647121fb
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:35:16 -0400

    Fr

--- catscripts/Government/United_Kingdom/UnitedKingdom.sh
+++ catscripts/Government/United_Kingdom/UnitedKingdom.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'United(| )Kingdom|\bUK\b|\bU\.K\b' newpages.txt | egrep -iv 'England|Scotland|Wales|Northern(| )Ireland' >> UnitedKingdom.txt 
+egrep -i 'United(| )Kingdom|\bUK\b|\bU\.K\b|Royaume(-| |)Uni|R-U' newpages.txt | egrep -iv 'England|Scotland|Wales|Northern(| )Ireland' >> UnitedKingdom.txt 
 
 UNITEDKINGDOM=`stat --print=%s UnitedKingdom.txt`
 
 if [ $UNITEDKINGDOM -ne 0 ];
 then
   export CATFILE="UnitedKingdom.txt"
-  export CATNAME="United Kingdom"
+  export CATNAME="Royaume-Uni"
   $CATEGORIZE
 fi
 

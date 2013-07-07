690f52c58f7668e6bf6fe9a86ac348e831a1f100
I am not me <impeccabletiming@gmail.com>
2013-07-07 15:18:35 -0400

    Update Guatemala.sh

--- catscripts/Government/Countries/Guatemala/Guatemala.sh
+++ catscripts/Government/Countries/Guatemala/Guatemala.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Guatemala|Guatamala' newpages.txt >> Guatemala.txt
+egrep -i 'Guat(e|é)mala|Guatamala' newpages.txt >> Guatemala.txt
 
 GUATEMALA=`stat --print=%s Guatemala.txt`
 
 if [ $GUATEMALA -ne 0 ];
 then
   export CATFILE="Guatemala.txt"
-  export CATNAME="Guatemala"
+  export CATNAME="Guatémala"
   $CATEGORIZE
 fi
 

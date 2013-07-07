962ed1c180e094887f55a0e6ac78c4931ed5a097
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:53:41 -0400

    Update Chile.sh

--- catscripts/Government/Countries/Chile/Chile.sh
+++ catscripts/Government/Countries/Chile/Chile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i '\bChile\b|Chilean' newpages.txt >> Chile.txt
+egrep -i '\bChili\b|Chilean' newpages.txt >> Chile.txt
 
 CHILE=`stat --print=%s Chile.txt`
 
 if [ $CHILE -ne 0 ];
 then
   export CATFILE="Chile.txt"
-  export CATNAME="Chile"
+  export CATNAME="Chili"
   $CATEGORIZE
 fi
 

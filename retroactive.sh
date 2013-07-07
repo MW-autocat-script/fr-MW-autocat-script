9b6f4568faac643089a69da1ab3ded3bc2bb0038
I am not me <impeccabletiming@gmail.com>
2013-07-06 23:13:45 -0400

    Add modified version of category start script to do a giant run through the whole site

---
+++ retroactive.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+date > lastrun
+#Export settings. Copy settings.conf.example to settings.conf as a starting point
+
+while read x; do
+  export $x
+done < settings.conf
+#Fetch new pages
+PIDFILE="pidfile.lock"
+if [ -e "$PIDFILE" ]; then
+PID=`cat $PIDFILE`
+if kill -0 $PID > /dev/null 2>&1; then
+printf 'Already running\n'
+exit 1
+else
+rm $PIDFILE
+fi
+fi
+echo $$ > $PIDFILE
+
+export CATEGORIZE="./util/Categorize.sh"
+#GET http://cfaj.freeshell.org/ipaddr.cgi > address.txt
+
+#while read y; do
+#  python $PYWIKIPEDIADIR/add_text.py -page:"User:$BOTNAME" -text:$y -except:$y -always
+#done < address.txt  
+
+python $PYWIKIPEDIADIR/pagegenerators.py -start:. -pt:1 |sed s'|  [0-9][0-9][0-9][0-9][0-9][0-9]: ||'|sed s'|  [0-9][0-9][0-9][0-9][0-9]: ||'|sed s'|  [0-9][0-9][0-9][0-9]: ||'  | sed s'|  [0-9][0-9][0-9]: ||' | sed s'|  [0-9][0-9]: ||' | sed s'|  [0-9]: ||' > newpages.txt
+ 
+./catscripts/Entertainment/Entertainment.sh
+#./catscripts/Government/Government.sh
+#./catscripts/Language/Language.sh
+#./catscripts/Lifestyle/Lifestyle.sh
+#./catscripts/Maintenance/Maintenance.sh
+#./catscripts/Math/Math.sh
+#./catscripts/Science/Science.sh
+#./catscripts/Technology/Technology.sh
+#./catscripts/Temp/Temp.sh
+#./catscripts/Transportation/Transportation.sh
+
+#Remove questions that have interrogative words from category 'Page titles lacking an interrogative word'
+
+#./util/noninterrogative.sh
+
+rm $PIDFILE

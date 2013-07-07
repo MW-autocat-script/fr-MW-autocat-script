a3704e6f94c9b6b25cd2c5af2137100f0c41e6c6
I am not me <impeccabletiming@gmail.com>
2013-07-07 15:38:20 -0400

    Translate to French

--- catscripts/Government/Countries/Israel/Israel.sh
+++ catscripts/Government/Countries/Israel/Israel.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-egrep -i 'Israel' newpages.txt | egrep -iv 'Jeru(-|)salem|Tel(| )Aviv|Israelite' >> Israel.txt
-egrep -i 'Israelite|Ancient Israel' newpages.txt >> AncientIsrael.txt
+egrep -i 'Isra(e|ë)l' newpages.txt | egrep -iv 'Jeru(-|)salem|Tel(| )Aviv|Israelite' >> Israel.txt
 egrep -i 'Jeru(-|)salem' newpages.txt | egrep -iv 'Jerusalem artichoke' >> Jerusalem.txt #Jerusalem artichoke is a tuber from a biannual sunflower, and has nothing to do with the city or Israel
 egrep -i 'Tel(| )Aviv' newpages.txt >> TelAviv.txt
 
 ISRAEL=`stat --print=%s Israel.txt`
-ISRAELITES=`stat --print=%s AncientIsrael.txt`
 JERUSALEM=`stat --print=%s Jerusalem.txt`
 TELAVIV=`stat --print=%s TelAviv.txt`
 
 if [ $ISRAEL -ne 0 ];
 then
   export CATFILE="Israel.txt"
-  export CATNAME="Israel"
-  $CATEGORIZE
-fi
-
-if [ $ISRAELITES -ne 0 ];
-then
-  export CATFILE="AncientIsrael.txt"
-  export CATNAME="Ancient Israel"
+  export CATNAME="Israël"
   $CATEGORIZE
 fi
 
@@ -39,6 +30,5 @@ then
 fi
 
 rm Israel.txt
-rm AncientIsrael.txt
 rm Jerusalem.txt
 rm TelAviv.txt

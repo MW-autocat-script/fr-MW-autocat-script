3e68e3c002c0c30669649edeffa0807499925c68
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 14:41:19 -0400

    Fr

--- catscripts/Government/Countries/Egypt/Egypt.sh
+++ catscripts/Government/Countries/Egypt/Egypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-KEYWORDS_EGYPT="Egypt(|s|ian)"
-KEYWORDS_ANCIENTEGYPYT="ancient Egypt|Tutankhamen|Tutencarmoon|tootencarmoon"
+KEYWORDS_EGYPT="(É|E)gypte"
+KEYWORDS_ANCIENTEGYPYT="l'(É|E)gypte Ancienne|Tutankhamen|Tutencarmoon|tootencarmoon"
 KEYWORDS_NILERIVER="Nile River|River Nile|Nile delta|Aswan dam"
 
 egrep -i "$KEYWORDS_EGYPT" newpages.txt | egrep -iv "$KEYWORDS_ANCIENTEGYPYT|$KEYWORDS_NILERIVER" >> Egypt.txt
@@ -15,14 +15,14 @@ NILE=`stat --print=%s NileRiver.txt`
 if [ $EGYPT -ne 0 ];
 then
   export CATFILE="Egypt.txt"
-  export CATNAME="Egypt"
+  export CATNAME="Egypte"
   $CATEGORIZE
 fi
 
 if [ $ANCIENT -ne 0 ];
 then
   export CATFILE="AncientEgypt.txt"
-  export CATNAME="Ancient Egypt"
+  export CATNAME="Égypte Ancienne"
   $CATEGORIZE
 fi
 

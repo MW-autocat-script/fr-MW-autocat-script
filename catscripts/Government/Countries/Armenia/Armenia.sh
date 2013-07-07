05d6394a4f45c6f4ae6a20414b2074fd6416f17d
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:43:03 -0400

    Fr

--- catscripts/Government/Countries/Armenia/Armenia.sh
+++ catscripts/Government/Countries/Armenia/Armenia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Armenia' newpages.txt >> Armenia.txt
+egrep -i 'Arm(é|e)nie' newpages.txt >> Armenia.txt
 
 ARMENIA=`stat --print=%s Armenia.txt`
 
 if [ $ARMENIA -ne 0 ];
 then
   export CATFILE="Armenia.txt"
-  export CATNAME="Armenia"
+  export CATNAME="Arménie"
   $CATEGORIZE
 fi
 

d3a527f32153a79b165448e63b1d9d81f02d63eb
I am not me <impeccabletiming@gmail.com>
2013-07-07 15:22:43 -0400

    Update Indonesia.sh

--- catscripts/Government/Countries/Indonesia/Indonesia.sh
+++ catscripts/Government/Countries/Indonesia/Indonesia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Indonesia(|s)\b|Sulawesi' newpages.txt >> Indonesia.txt
+egrep -i 'Indon(e|é)sie\b|Sulawesi' newpages.txt >> Indonesia.txt
 
 INDONESIA=`stat --print=%s Indonesia.txt`
 
 if [ $INDONESIA -ne 0 ];
 then
   export CATFILE="Indonesia.txt"
-  export CATNAME="Indonesia"
+  export CATNAME="Indonésie"
   $CATEGORIZE
 fi
 

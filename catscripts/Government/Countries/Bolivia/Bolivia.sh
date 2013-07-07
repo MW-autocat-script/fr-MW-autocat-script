936919de536c3043473a5a2e3de465f04875256b
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:49:20 -0400

    Fr

--- catscripts/Government/Countries/Bolivia/Bolivia.sh
+++ catscripts/Government/Countries/Bolivia/Bolivia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i "Bolivia" newpages.txt > Bolivia.txt
+egrep -i "Bolivie" newpages.txt > Bolivia.txt
 
 BOLIVIA=`stat --print=%s Bolivia.txt`
 
 if [ $BOLIVIA -ne 0 ];
 then
   export CATFILE="Bolivia.txt"
-  export CATNAME="Bolivia"
+  export CATNAME="Bolivie"
   $CATEGORIZE
 fi
 

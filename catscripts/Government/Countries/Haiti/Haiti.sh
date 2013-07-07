9cdb82559e3d1e3ea2e50549df46117622b6fe41
I am not me <impeccabletiming@gmail.com>
2013-07-07 15:20:03 -0400

    Update Haiti.sh

--- catscripts/Government/Countries/Haiti/Haiti.sh
+++ catscripts/Government/Countries/Haiti/Haiti.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Haiti' newpages.txt >> Haiti.txt
+egrep -i 'Ha(i|ï)ti' newpages.txt >> Haiti.txt
 
 HAITI=`stat --print=%s Haiti.txt`
 
 if [ $HAITI -ne 0 ];
 then
   export CATFILE="Haiti.txt"
-  export CATNAME="Haiti"
+  export CATNAME="Haïti"
   $CATEGORIZE
 fi
 

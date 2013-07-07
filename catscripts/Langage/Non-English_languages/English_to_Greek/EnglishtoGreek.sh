ea09c5d78a5046f6809d2862825f21b51153bafa
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 15:28:25 -0400

    Update EnglishtoGreek.sh

--- catscripts/Langage/Non-English_languages/English_to_Greek/EnglishtoGreek.sh
+++ catscripts/Langage/Non-English_languages/English_to_Greek/EnglishtoGreek.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Greek word for|How do you say .+ in Greek' newpages.txt >> EnglishtoGreek.txt
+egrep -i 'Mot grecque|Comment tu di.+ en grecque' newpages.txt >> EnglishtoGreek.txt
 
 GREEK=`stat --print=%s EnglishtoGreek.txt`
 
 if [ $GREEK -ne 0 ];
 then
   export CATFILE="EnglishtoGreek.txt"
-  export CATNAME="English to Greek"
+  export CATNAME="Français à grecque"
   $CATEGORIZE
 fi
 

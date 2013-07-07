d28f92bec12d78bf652d1fa7ae2cc7f238f1ac52
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 13:51:02 -0400

    Fr

--- catscripts/Government/Countries/Cameroon/Cameroon.sh
+++ catscripts/Government/Countries/Cameroon/Cameroon.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Cameroon' newpages.txt >> Cameroon.txt
+egrep -i 'Cameroun' newpages.txt >> Cameroon.txt
 
 CAMEROON=`stat --print=%s Cameroon.txt`
 
 if [ $CAMEROON -ne 0 ];
 then
   export CATFILE="Cameroon.txt"
-  export CATNAME="Cameroon"
+  export CATNAME="Cameroun"
   $CATEGORIZE
 fi
 

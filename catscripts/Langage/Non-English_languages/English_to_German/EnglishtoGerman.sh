8cf72fbb18a9c5e092617922f1b607c5fb1861e1
Jr Mime <vinc.bel@hotmail.com>
2013-07-07 15:27:06 -0400

    Update EnglishtoGerman.sh

--- catscripts/Langage/Non-English_languages/English_to_German/EnglishtoGerman.sh
+++ catscripts/Langage/Non-English_languages/English_to_German/EnglishtoGerman.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-egrep -i 'How do you say.+in German\]\]' newpages.txt >> EnglishtoGerman.txt
-egrep -i 'How do you spell.+in German\]\]' newpages.txt >> EnglishtoGerman.txt
-egrep -i '[abcdefghijklmopqrstuvwxyz]{1,} in German\]\]' newpages.txt >> EnglishtoGerman.txt
-egrep -i 'What is the German word for' newpages.txt >> EnglishtoGerman.txt
-egrep -i 'How to say.+in German' newpages.txt >> EnglishtoGerman.txt
+egrep -i 'Comment tu di.+en alleman\]\]' newpages.txt >> EnglishtoGerman.txt
+egrep -i "Comment t'écrit.+en alleman\]\]" newpages.txt >> EnglishtoGerman.txt
+egrep -i '[abcdefghijklmopqrstuvwxyz]{1,} en alleman\]\]' newpages.txt >> EnglishtoGerman.txt
+egrep -i 'Mot alleman' newpages.txt >> EnglishtoGerman.txt
 
 ENGLISHTOGERMAN=`stat --print=%s EnglishtoGerman.txt`
 
 if [ $ENGLISHTOGERMAN -ne 0 ];
 then
   export CATFILE="EnglishtoGerman.txt"
-  export CATNAME="English to German"
+  export CATNAME="Français à allemand"
   $CATEGORIZE
 fi
 

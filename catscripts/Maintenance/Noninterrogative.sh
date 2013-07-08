4872d953023a6f237717a74335fc04f9466226d1
I am not me <impeccabletiming@gmail.com>
2013-07-08 18:40:14 -0400

    Minor update to non-interrogative and Pornography

--- catscripts/Maintenance/Noninterrogative.sh
+++ catscripts/Maintenance/Noninterrogative.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat newpages.txt | egrep -iv "\bqui\b|\bquoi\b|\boù\b|\bquand\b|\bpourquoi\b|\bcomment\b|\best-ce\b|\bétait\b|\bquel\b|\bpeut-t\b|\bcombien\b|\bdurant\b|\bA |\bD'ou\b|\bD' ou\b|\bD ou\b|\bD oû\b|\bD'oû\b|\bDou \b|\bDans un\b|\bDans qu\b|\bQui est\b|\best-t\b|\best t\b|\best il\b|\best-il\b|\bcoment\b|\bcomment\b|\bcommen\b|\bcombien\b|\bQue\b|\bQu'\b|\bQuant\b" >> noninterrogative.txt
+cat newpages.txt | egrep -iv "\bqui\b|\bquoi\b|\boù\b|\bquand\b|\bpourquoi\b|\bcomment\b|\best-ce\b|\bétait\b|\bquel\b|\bpeut-t\b|\bcombien\b|\bdurant\b|\bA |\bD'ou\b|\bD' ou\b|\bD ou\b|\bD oû\b|\bD'oû\b|\bDou \b|\boù\b|\bDans un\b|\bDans qu\b|\bQui est\b|\best-t\b|\best t\b|\best il\b|\best-il\b|\bcoment\b|\bcomment\b|\bcommen\b|\bcombien\b|\bQue\b|\bQu'\b|\bQuant\b" >> noninterrogative.txt
 
 NONINT=`stat --print=%s noninterrogative.txt`
 

2390a1d98a9d63142d408441f3a44ccddcd29193
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 21:30:14 -0400

    Update Wales.sh

--- catscripts/Government/Countries/Wales/Wales.sh
+++ catscripts/Government/Countries/Wales/Wales.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Wales' newpages.txt | egrep -iv 'Jim(|bo|my)(| )Wales|New South Wales' >> Wales.txt
+egrep -i 'Pays de Galles' newpages.txt | egrep -iv 'Jim(|bo|my)(| )Wales|New South Wales' >> Wales.txt
 
 WALES=`stat --print=%s Wales.txt`
 
 if [ $WALES -ne 0 ];
 then
   export CATFILE="Wales.txt"
-  export CATNAME="Wales"
+  export CATNAME="Pays de Galles"
   $CATEGORIZE
 fi
 

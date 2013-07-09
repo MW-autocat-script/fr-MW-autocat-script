739d991f0ef9d422d79a5bb2f95155e29d3ab5bd
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 20:18:39 -0400

    Update Nepal.sh

--- catscripts/Government/Countries/Nepal/Nepal.sh
+++ catscripts/Government/Countries/Nepal/Nepal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Nepal' newpages.txt >> Nepal.txt
+egrep -i 'Népal' newpages.txt >> Nepal.txt
 
 NEPAL=`stat --print=%s Nepal.txt`
 
 if [ $NEPAL -ne 0 ];
 then
   export CATFILE="Nepal.txt"
-  export CATNAME="Nepal"
+  export CATNAME="Népal"
   $CATEGORIZE
 fi
 

4a76e904d2eb818afba9d49f257591994083b683
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 20:06:32 -0400

    Fr

--- catscripts/Government/Countries/Iceland/Iceland.sh
+++ catscripts/Government/Countries/Iceland/Iceland.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i '\bIceland' newpages.txt | egrep -iv 'Icelandic' > Iceland.txt
+egrep -i 'Islande' newpages.txt | egrep -iv 'Icelandic' > Iceland.txt
 
 ICELAND=`stat --print=%s Iceland.txt`
 
 if [ $ICELAND -ne 0 ];
 then
   export CATFILE="Iceland.txt"
-  export CATNAME="Iceland"
+  export CATNAME="Islande"
   $CATEGORIZE
 fi
 

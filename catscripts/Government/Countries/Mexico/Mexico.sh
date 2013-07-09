5a15f47a0cf17971e758e8026826852ad4288f2b
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 20:16:10 -0400

    Update Mexico.sh

--- catscripts/Government/Countries/Mexico/Mexico.sh
+++ catscripts/Government/Countries/Mexico/Mexico.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Mexic(o|an)|Tijuana' newpages.txt | egrep -iv 'New(| )Mexic(o|an)' > Mexico.txt
+egrep -i 'Mexique|Tijuana' newpages.txt | egrep -iv 'Nouveau Mexique' > Mexico.txt
 
 MEXICO=`stat --print=%s Mexico.txt`
 
 if [ $MEXICO -ne 0 ];
 then
   export CATFILE="Mexico.txt"
-  export CATNAME="Mexico"
+  export CATNAME="Mexique"
   $CATEGORIZE
 fi
 

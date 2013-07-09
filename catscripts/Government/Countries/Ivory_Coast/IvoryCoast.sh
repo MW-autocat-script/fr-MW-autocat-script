a8db333d1bea85f88eb7636f718e9b4e8d32339f
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 20:21:24 -0400

    Update IvoryCoast.sh

--- catscripts/Government/Countries/Ivory_Coast/IvoryCoast.sh
+++ catscripts/Government/Countries/Ivory_Coast/IvoryCoast.sh
@@ -7,7 +7,7 @@ IVORYCOAST=`stat --print=%s IvoryCoast.txt`
 if [ $IVORYCOAST -ne 0 ];
 then
   export CATFILE="IvoryCoast.txt"
-  export CATNAME="Côte-d'Ivoire"
+  export CATNAME="Côte d'Ivoire"
   $CATEGORIZE
 fi
 

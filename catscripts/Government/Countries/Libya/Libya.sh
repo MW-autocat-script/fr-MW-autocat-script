e032a97db2f8e59cce753ceccbba98f24b86c961
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 20:11:16 -0400

    Update Libya.sh

--- catscripts/Government/Countries/Libya/Libya.sh
+++ catscripts/Government/Countries/Libya/Libya.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Libya|Ghadafi' newpages.txt >> Libya.txt
+egrep -i 'Libye|Ghadafi' newpages.txt >> Libya.txt
 
 LIBYA=`stat --print=%s Libya.txt`
 
 if [ $LIBYA -ne 0 ];
 then
   export CATFILE="Libya.txt"
-  export CATNAME="Libya"
+  export CATNAME="Libye"
   $CATEGORIZE
 fi
 

1a6fe0e0ebbfd5efe370137127da4d8e91200a20
Jr Mime <vinc.bel@hotmail.com>
2013-07-08 21:23:39 -0400

    Update SouthKorea.sh

--- catscripts/Government/Countries/South_Korea/SouthKorea.sh
+++ catscripts/Government/Countries/South_Korea/SouthKorea.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-egrep -i 'South Korea|Seoul' newpages.txt >> SouthKorea.txt
-egrep -i 'Korea\b' newpages.txt | egrep -iv 'North(| )Korea' >> SouthKorea.txt #Questions that ask about "Korea" are probably referring to South Korea
+egrep -i 'Corée du Sud' newpages.txt >> SouthKorea.txt
+egrep -i 'Corée\b' newpages.txt | egrep -iv 'Corée du Nord' >> SouthKorea.txt #Questions that ask about "Korea" are probably referring to South Korea
 
 SKOREA=`stat --print=%s SouthKorea.txt`
 
 if [ $SKOREA -ne 0 ];
 then
   export CATFILE="SouthKorea.txt"
-  export CATNAME="South Korea"
+  export CATNAME="Corée du Sud"
   $CATEGORIZE
 fi
 

a78729c28205e537cb2d48c2602f96e876e0b9ee
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 00:30:05 +0200

    Tanzia ? Let's assume it's Tanzania...

--- catscripts/Government/Countries/Tanzia/Tanzia.sh
+++ catscripts/Government/Countries/Tanzia/Tanzia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Tanzia' newpages.txt >> Tanzia.txt
+egrep -i 'Tanzani(a|e)' newpages.txt >> Tanzia.txt
 
 TANZIA=`stat --print=%s Tanzia.txt`
 
 if [ $TANZIA -ne 0 ];
 then
   export CATFILE="Tanzia.txt"
-  export CATNAME="Tanzia"
+  export CATNAME="Tanzanie"
   $CATEGORIZE
 fi
 

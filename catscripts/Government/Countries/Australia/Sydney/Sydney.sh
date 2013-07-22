4ceb535ac8a05a84277042d697ec06a82fc02911
Wyz83 <olivier.wyz@gmail.com>
2013-07-22 21:16:14 +0200

    Update Sydney.sh

--- catscripts/Government/Countries/Australia/Sydney/Sydney.sh
+++ catscripts/Government/Countries/Australia/Sydney/Sydney.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-egrep -i 'Sydney(,|)(| )Australia|Sydney Harbo(u|)r|Australia.+Sydney|Sydney.+Australia' newpages.txt | egrep -iv 'Sydney Opera House' >> Sydney.txt
+egrep -i 'Sydney(,|)(| )Australie|Port Jackson|Baie (|de) Sydney|Port (|de )Sydney|Australie.+Sydney|Sydney.+Australie' newpages.txt | egrep -iv 'Sydney Opera House|Opéra de Sydney' >> Sydney.txt
 egrep -i 'Sydney(| )Opera(| )House' newpages.txt >> SydneyOperaHouse.txt
 
 SYDNEY=`stat --print=%s Sydney.txt`
@@ -9,14 +9,14 @@ OPERA=`stat --print=%s SydneyOperaHouse.txt`
 if [ $SYDNEY -ne 0 ];
 then
   export CATFILE="Sydney.txt"
-  export CATNAME="Sydney, Australia"
+  export CATNAME="Sydney, Australie"
   $CATEGORIZE
 fi
 
 if [ $OPERA -ne 0 ];
 then
   export CATFILE="SydneyOperaHouse.txt"
-  export CATNAME="Sydney Opera House"
+  export CATNAME="Opéra de Sydney"
   $CATEGORIZE
 fi
 

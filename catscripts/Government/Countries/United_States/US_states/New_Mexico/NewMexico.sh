01655b5c1ea17fafd600db12171155a8569f8163
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 00:12:45 +0200

    Update NewMexico.sh

--- catscripts/Government/Countries/United_States/US_states/New_Mexico/NewMexico.sh
+++ catscripts/Government/Countries/United_States/US_states/New_Mexico/NewMexico.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'New(| )Mexico|New(| )Mexican' newpages.txt >> NewMexico.txt
+egrep -i 'New(| )Mexico|New(| )Mexican|Nouveau(-| )Mexique' newpages.txt >> NewMexico.txt
 
 NEWMEXICO=`stat --print=%s NewMexico.txt`
 
 if [ $NEWMEXICO -ne 0 ];
 then
   export CATFILE="NewMexico.txt"
-  export CATNAME="New Mexico"
+  export CATNAME="Nouveau-Mexique"
   $CATEGORIZE
 fi
 

de1d3c408fb02a3aeaefcba3dee0abdab1592672
Wyz83 <olivier.wyz@gmail.com>
2013-07-22 23:45:03 +0200

    Update USmilitary.sh

--- catscripts/Government/Countries/United_States/US_military/USmilitary.sh
+++ catscripts/Government/Countries/United_States/US_military/USmilitary.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-egrep -i '(Marine|Marines|)(US|U\.S\.|United States|EU|E\.U\.) Marine(|s) (|Corps)|USMC' newpages.txt >> USMC.txt
-egrep -i 'Armée (US|U\.S\.|United States|EU|E\.U\.)' newpages.txt >> USArmy.txt
-egrep -i "Armée aérienne (des|)( |)(US|U\.S\.|United States|EU|E\.U\.)|USAF\b" newpages.txt >> USAF.txt
-egrep -i "Armée marine (des|)( |)(US|U\.S\.|United States|EU|E\.U\.)|\bUSN\b" newpages.txt >> USNavy.txt
-egrep -i "Garde côtière (des|)( |)(US|U\.S\.|United States|EU|E\.U\.)|USCG" newpages.txt >> USCG.txt
-egrep -i '\bdod\b|Département de Défen(c|s)e|Armée militaire (US|U\.S\.|United States|EU|E\.U\.)' newpages.txt | egrep -iv 'Lott Dod|Army|Navy|Air Force|Navy|Coast Guard|Marine' >> USMilitary.txt
+egrep -i '(Marine|Marines|)(US|U\.S\.|United States|EU|E\.U\.|américaine) Marine(|s) (|Corps)|USMC' newpages.txt >> USMC.txt
+egrep -i 'Armée (US|U\.S\.|United States|EU|E\.U\.|américaine)' newpages.txt >> USArmy.txt
+egrep -i "Armée aérienne (des|)( |)(US|U\.S\.|United States|EU|E\.U\.|américaine)|USAF\b" newpages.txt >> USAF.txt
+egrep -i "Armée marine (des|)( |)(US|U\.S\.|United States|EU|E\.U\.|américaine)|\bUSN\b" newpages.txt >> USNavy.txt
+egrep -i "Garde(-| )côtes (des|)( |)(US|U\.S\.|United States|EU|E\.U\.|américain(e|s))|USCG" newpages.txt >> USCG.txt
+egrep -i '\bdod\b|Département de Défen(c|s)e|Forces armées (US|U\.S\.|United States|EU|E\.U\.|américaine(s))' newpages.txt | egrep -iv 'Lott Dod|Army|Navy|Air Force|Navy|Coast Guard|Marine' >> USMilitary.txt
 
 USMC=`stat --print=%s USMC.txt`
 USARMY=`stat --print=%s USArmy.txt`
@@ -52,7 +52,7 @@ fi
 if [ $USMILITARY -ne 0 ];
 then
   export CATFILE="USMilitary.txt"
-  export CATNAME="Armée militaire des États-Unis"
+  export CATNAME="Forces armées des États-Unis"
   $CATEGORIZE
 fi
 

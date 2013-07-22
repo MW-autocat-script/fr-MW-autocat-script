b6c893ef265d082fcac0fd382070bb46f5dc96a6
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 00:53:36 +0200

    Update Italy.sh

--- catscripts/Government/Countries/Italy/Italy.sh
+++ catscripts/Government/Countries/Italy/Italy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-egrep -i 'Italy|Italie' newpages.txt | egrep -iv 'Rome|Pompeii|Pisa|Venice' >> Italy.txt 
-egrep -i '\bRome(|s)\b' newpages.txt | egrep -iv 'Rome antique|Rome(,|)(| )Georgia' >> Rome.txt
-egrep -i "Romain antique|Romains|époque romaine|Auguste César|l'empereur Néron|Caligula" newpages.txt | egrep -iv 'Livre des Romains|Romains [0-9]{1,}\:[0-9]{1,}|Julius Caesar' >> AncientRome.txt #Exclude biblical book and Julius Caesar
+egrep -i 'Italy|Italie' newpages.txt | egrep -iv 'Rome|Pompeii|Pise|Venise' >> Italy.txt 
+egrep -i '\bRome(|s)\b' newpages.txt | egrep -iv 'Rome antique|Rome(,|)(| )Géorgie' >> Rome.txt
+egrep -i "Rome antique|Romains|époque romaine|Auguste César|(empereur )Néron|Caligula" newpages.txt | egrep -iv 'Livre des Romains|Romains [0-9]{1,}\:[0-9]{1,}|Julius Caesar|Jules César' >> AncientRome.txt #Exclude biblical book and Julius Caesar
 egrep -i 'Julius Caesar|Jules César' newpages.txt >> JuliusCaesar.txt
-egrep -i 'Pisa' newpages.txt >> Pisa.txt
-egrep -i 'Pompeii' newpages.txt >> Pompeii.txt
-egrep -i 'Venice' newpages.txt >> Venice.txt
+egrep -i 'Pise' newpages.txt >> Pisa.txt
+egrep -i 'Pompéi' newpages.txt >> Pompeii.txt
+egrep -i 'Venise|gondole' newpages.txt >> Venice.txt
 
 ITALY=`stat --print=%s Italy.txt`
 ROME=`stat --print=%s Rome.txt`
@@ -26,28 +26,28 @@ fi
 if [ $ROME -ne 0 ];
 then
   export CATFILE="Rome.txt"
-  export CATNAME="Rome"
+  export CATNAME="Rome, Italie"
   $CATEGORIZE
 fi
 
 if [ $ANCIENTROME -ne 0 ];
 then
   export CATFILE="AncientRome.txt"
-  export CATNAME="Romain antuque"
+  export CATNAME="Rome antique"
   $CATEGORIZE
 fi
 
 if [ $PISA -ne 0 ];
 then
   export CATFILE="Pisa.txt"
-  export CATNAME="Pisa, Italy"
+  export CATNAME="Pise, Italie"
   $CATEGORIZE
 fi
 
 if [ $POMPEII -ne 0 ];
 then
   export CATFILE="Pompeii.txt"
-  export CATNAME="Pompeii"
+  export CATNAME="Pompéi"
   $CATEGORIZE
 fi
 
@@ -61,7 +61,7 @@ fi
 if [ $VENICE -ne 0 ];
 then
   export CATFILE="Venice.txt"
-  export CATNAME="Venice"
+  export CATNAME="Venise"
   $CATEGORIZE
 fi
 

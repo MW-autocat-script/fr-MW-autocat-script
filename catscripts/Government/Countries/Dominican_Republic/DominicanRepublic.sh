7eeec81c99bf9e2dc59b536e25352df142a6f1a4
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 01:12:11 +0200

    Update DominicanRepublic.sh

--- catscripts/Government/Countries/Dominican_Republic/DominicanRepublic.sh
+++ catscripts/Government/Countries/Dominican_Republic/DominicanRepublic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'R(é|e)publique Dominicane' newpages.txt >> DominicanRepublic.txt
+egrep -i 'R(é|e)publique dominicaine' newpages.txt >> DominicanRepublic.txt
 
 DOMINICAN=`stat --print=%s DominicanRepublic.txt`
 
 if [ $DOMINICAN -ne 0 ];
 then
   export CATFILE="DominicanRepublic.txt"
-  export CATNAME="République Dominicane"
+  export CATNAME="République dominicaine"
   $CATEGORIZE
 fi
 

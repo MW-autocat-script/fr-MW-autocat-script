493a67d92bd790794c0df5325fef73e2bb9474af
Wyz83 <olivier.wyz@gmail.com>
2013-07-22 21:27:43 +0200

    New South Wales (Australy) and Jimmy Wales (Wikipedia and Wikia) have nothing to do here...

--- catscripts/Government/Countries/Wales/Wales.sh
+++ catscripts/Government/Countries/Wales/Wales.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-egrep -i 'Pays de Galles' newpages.txt | egrep -iv 'Jim(|bo|my)(| )Wales|New South Wales' >> Wales.txt
+egrep -i 'Pays de Galles' newpages.txt >> Wales.txt
 
 WALES=`stat --print=%s Wales.txt`
 

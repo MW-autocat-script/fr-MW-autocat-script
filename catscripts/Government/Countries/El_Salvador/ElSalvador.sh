3fe038b8b470f1c670093976af066155e18f3343
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 01:08:47 +0200

    Update ElSalvador.sh

--- catscripts/Government/Countries/El_Salvador/ElSalvador.sh
+++ catscripts/Government/Countries/El_Salvador/ElSalvador.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-egrep -i 'El(| )Salvador' newpages.txt >> ElSalvador.txt
+egrep -i 'Salvador' newpages.txt >> ElSalvador.txt
 
 ELSALVADOR=`stat --print=%s ElSalvador.txt`
 
 if [ $ELSALVADOR -ne 0 ];
 then
   export CATFILE="ElSalvador.txt"
-  export CATNAME="El Salvador"
+  export CATNAME="Salvador"
   $CATEGORIZE
 fi
 

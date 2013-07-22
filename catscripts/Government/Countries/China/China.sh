c3c2b80b9cdb57e02a4ea97b87f47b095ebab231
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 01:18:16 +0200

    Update China.sh

--- catscripts/Government/Countries/China/China.sh
+++ catscripts/Government/Countries/China/China.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 egrep -i "République de Chine" newpages.txt >> China.txt
-egrep -i 'Chine' newpages.txt | egrep -iv 'République de Chine|fine(| )china|antique(| )china|M(u|û)r de Chine|Beijing|Hong(| )Kong' >> China.txt #There are two "Republics of China". If one is not named  explicitly, presume the question is about the People's Republic, as it is larger and more internationally recognized
-egrep -i "M(u|û)r de Chine" newpages.txt >> GreatWall.txt
-egrep -i "Beijing" newpages.txt >> Beijing.txt
+egrep -i 'Chine' newpages.txt | egrep -iv 'République de Chine|fine(| )china|princess of China|chine(| )antique|Muraille de Chine|Beijing|Pékin|Hong(| )Kong' >> China.txt #There are two "Republics of China". If one is not named  explicitly, presume the question is about the People's Republic, as it is larger and more internationally recognized
+egrep -i "Muraille de Chine" newpages.txt >> GreatWall.txt
+egrep -i "Beijing|Pékin" newpages.txt >> Beijing.txt
 egrep -i "Hong(| )Kong" newpages.txt >> HongKong.txt 
 
 CHINA=`stat --print=%s China.txt`
@@ -21,14 +21,14 @@ fi
 if [ $WALL -ne 0 ];
 then
   export CATFILE="GreatWall.txt"
-  export CATNAME="Mûr de Chine"
+  export CATNAME="Muraille de Chine"
   $CATEGORIZE
 fi
 
 if [ $BEIJING -ne 0 ];
 then
   export CATFILE="Beijing.txt"
-  export CATNAME="Beijing"
+  export CATNAME="Pékin"
   $CATEGORIZE
 fi
 

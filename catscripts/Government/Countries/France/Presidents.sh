82b470b0639b41879a25d71d81173194a3466f67
Wyz83 <olivier.wyz@gmail.com>
2013-07-23 23:13:08 +0200

    Update Presidents.sh

--- catscripts/Government/Countries/France/Presidents.sh
+++ catscripts/Government/Countries/France/Presidents.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-egrep -i 'France.+pr(e|é)sident|pr(e|é)sident.+France|Lois.+Napoléon(| )Bonaparte|Adolphe Thiers|Patrice de Mac-Mahon, duc de Magenta|Jules Grévy|Marie François Sadi Carnot|Jean Casimir-Perier|F(e|é)lix Faure|(É|E)mile Loubet|Armand Falli(è|e)res|Raymond Poincar(é|e)|Paul Deschanel|Alexandre Millerand|Gaston Doumergue|Paul Doumer|Albert Lebrun|Vincent Auriol|René Coty|Charles de Gaulle|Alain Poher|Georges Pompidou|Val(é|e)ry Giscard d'Estaing|Fran(ç|c)ois Mitterrand|Jacques Chirac|Nicolas Sarkozy|François Hollande' newpages.txt >> French presidents.txt
+egrep -i 'France.+pr(e|é)sident|pr(e|é)sident.+France|Louis.+Napoléon(| )Bonaparte|Adolphe Thiers|Patrice de Mac-Mahon|duc de Magenta|Jules Grévy|Marie François Sadi Carnot|Jean Casimir-Perier|F(e|é)lix Faure|(É|E)mile Loubet|Armand Falli(è|e)res|Raymond Poincar(é|e)|Paul Deschanel|Alexandre Millerand|Gaston Doumergue|Paul Doumer|Albert Lebrun|Vincent Auriol|René Coty|Charles de Gaulle|Alain Poher|Georges Pompidou|Val(é|e)ry Giscard d''Estaing|Fran(ç|c)ois Mitterrand|Jacques Chirac|Nicolas Sarkozy|Fran(ç|c)ois Hollande' newpages.txt >> FrenchPresidents.txt
 
-FRENCH PRESIDENTS=`stat --print=%s French presidents.txt`
+FRENCH_PRESIDENTS=`stat --print=%s FrenchPresidents.txt`
 
-if [ $FRANCE -ne 0 ];
+if [ $FRENCH_PRESIDENTS -ne 0 ];
 then
-  export CATFILE="French presidents.txt"
+  export CATFILE="FrenchPresidents.txt"
   export CATNAME="Présidents de la République française"
   $CATEGORIZE
 fi
 
-rm French presidents.txt
+rm FrenchPresidents.txt

01d5a89674f33ce80696d14814a9904ffefa6f6c
Jr Mime <vinc.bel@hotmail.com>
2013-07-23 17:01:23 -0400

    Update Presidents.sh

--- catscripts/Government/Countries/France/Presidents.sh
+++ catscripts/Government/Countries/France/Presidents.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-egrep -i 'pr(e|é)sident.+France|Lois.+Napoléon(| )Bonaparte|Adolphe Thiers|Patrice de Mac-Mahon, duc de Magenta|Jules Grévy|Marie François Sadi Carnot|Jean Casimir-Perier|F(e|é)lix Faure|(É|E)mile Loubet|Armand Falli(è|e)res|Raymond Poincar(é|e)|Paul Deschanel|Alexandre Millerand|Gaston Doumergue|Paul Doumer|Albert Lebrun|Vincent Auriol|René Coty|Charles de Gaulle|Alain Poher|Georges Pompidou|Val(é|e)ry Giscard d'Estaing|Fran(ç|c)ois Mitterrand|Jacques Chirac|Nicolas Sarkozy|François Hollande' newpages.txt >> French presidents.txt
+egrep -i 'France.+pr(e|é)sident|pr(e|é)sident.+France|Lois.+Napoléon(| )Bonaparte|Adolphe Thiers|Patrice de Mac-Mahon, duc de Magenta|Jules Grévy|Marie François Sadi Carnot|Jean Casimir-Perier|F(e|é)lix Faure|(É|E)mile Loubet|Armand Falli(è|e)res|Raymond Poincar(é|e)|Paul Deschanel|Alexandre Millerand|Gaston Doumergue|Paul Doumer|Albert Lebrun|Vincent Auriol|René Coty|Charles de Gaulle|Alain Poher|Georges Pompidou|Val(é|e)ry Giscard d'Estaing|Fran(ç|c)ois Mitterrand|Jacques Chirac|Nicolas Sarkozy|François Hollande' newpages.txt >> French presidents.txt
 
 FRENCH PRESIDENTS=`stat --print=%s French presidents.txt`
 
